fec448fa2b69094ca14203fe6ad9933b714c07fd
Justin Ofori <justinkofori@gmail.com>
2021-05-20 16:36:24 -0700

    Create files, format basic calculator with buttons, add event listeners to manipulate data as user interacts with UI. Debug for user to string together several operations along with pressing the decimal before any numbers.

---
+++ README.md
@@ -0,0 +1,6 @@
+In this project I'll create a simple calculator including addition, subtraction, multiplication, and division operators.
+
+What I Learned:
+-Effectively using booleans to store data of how users interact with UI
+-toString(), parseInt(), and parseFloat() are very useful for manipulating numbers within html.
+-When parsing arguments between strings and floats, converting a float into a string will remove trailing zeros.
---
+++ script.js
@@ -0,0 +1,155 @@
+let firstNumber='0';
+let secondNumber=undefined; // secondNumber is undefined when first number is being typed
+
+let operator=''; // remember if/when operators are pressed
+let decimalIsPresentFirstNumber=false;
+let decimalIsPresentSecondNumber=false;
+
+//calculator functions below
+function add(x,y){
+    return parseFloat(x)+parseFloat(y);
+}
+
+function subtract(x,y){
+    return parseFloat(x)-parseFloat(y);
+}
+
+function multiply(x,y){
+    return parseFloat(x)*parseFloat(y);
+}
+function divide(x,y){
+    if(y==0){
+        return 'ERROR';
+    } else {
+        return parseFloat(x)/parseFloat(y);
+    }
+}
+
+function operate(operator,x,y){
+    if(operator=='add'){
+        firstNumber=add(x,y);
+        return firstNumber;
+    } else if(operator=='subtract'){
+        firstNumber=subtract(x,y);
+        return firstNumber;
+    } else if(operator=='multiply'){
+        firstNumber=multiply(x,y);
+        return firstNumber;
+    } else if(operator=='divide'){
+        firstNumber=divide(x,y);
+        return firstNumber;
+    }
+}
+
+function allClear(){
+    firstNumber=0;
+    secondNumber=undefined;
+    operator='';
+    displayValue.textContent='0';
+    decimalIsPresentFirstNumber=false;
+    decimalIsPresentSecondNumber=false;
+}
+//calculator functions above
+
+// for manipulating calculator display
+let displayValue = document.getElementById('displayValue');
+
+// for button number event listeners
+let numberArray = [...document.getElementsByClassName('numberButton')]
+
+// add event listeners to all numbers, changing the display value as buttons are pressed
+numberArray.forEach(element => {
+    element.addEventListener('click', function(){
+        
+        // instances for the first number
+        if(decimalIsPresentFirstNumber==true && secondNumber==undefined && operator==''){ // a digit of 1st number is placed after decimal
+            firstNumber+=element.textContent;
+            displayValue.textContent=firstNumber;
+        } else if(firstNumber=='0' && secondNumber==undefined && operator==''){ // 1st digit of 1st number replaces the zero on display
+            firstNumber=element.textContent;
+            displayValue.textContent=firstNumber;
+        } else if(decimalIsPresentFirstNumber==false && secondNumber==undefined && operator==''){ // next digit in 1st integer is placed
+            firstNumber+=element.textContent;
+            displayValue.textContent=firstNumber;
+        }
+        
+        // instances for the second number
+        if(secondNumber=='0.' && operator!=''){ // a digit of 2nd number is placed after a zero with decimal
+            secondNumber+=element.textContent;
+            displayValue.textContent=secondNumber;
+        } else if(decimalIsPresentSecondNumber==false && secondNumber==undefined && operator!=''){ // 1st digit of 2nd integer displays on screen
+            secondNumber=element.textContent;
+            displayValue.textContent=secondNumber;
+        } else if(operator!='' && displayValue.textContent==secondNumber){ // if operator is pressed and second number exists
+            secondNumber+=element.textContent;
+            displayValue.textContent=secondNumber;
+        }
+
+    });
+});
+
+// for operator button event listeners
+let operatorArray = [...document.getElementsByClassName('operatorButton')];
+
+// define operator value after an operator is clicked
+operatorArray.forEach(element => {
+        //if 2nd operator is clicked after 2 numbers are defined, calculate and redefine operator
+        element.addEventListener('click', function(){
+            if((typeof(firstNumber)=='string') && (typeof(secondNumber)=='string')){
+                firstNumber=operate(operator,firstNumber,secondNumber).toString();
+                displayValue.textContent=firstNumber;
+                secondNumber=undefined;
+                decimalIsPresentSecondNumber=false;
+                operator=element.id;
+            } else {    
+            operator=element.id; //if only one number is defined, define operator
+            }
+        });
+});
+
+// for equals button event listener
+let equalsButton = document.getElementById('equals');
+
+// only execute operate function on equal button press if operator & 2nd number is known
+equalsButton.addEventListener('click', function(){
+    if(operator!='' && secondNumber!=undefined){
+        firstNumber=operate(operator,firstNumber,secondNumber);
+        displayValue.textContent=firstNumber;
+        secondNumber=undefined;
+        decimalIsPresentSecondNumber=false;
+        operator='';
+    }
+});
+
+// for decimal button event listener
+let decimalButton = document.getElementById('decimal');
+
+decimalButton.addEventListener('click', function(){
+
+    if(decimalIsPresentFirstNumber==false && secondNumber==undefined){ // first number being typed can add its decimal
+        firstNumber+='.'
+        displayValue.textContent=firstNumber;
+        decimalIsPresentFirstNumber=true;
+        console.log('first number being typed can add its decimal')
+    } else if(decimalIsPresentSecondNumber==false && operator!='' && secondNumber==undefined){ // decimal is placed for second number
+        secondNumber='0.';                                                                     // before any digits
+        displayValue.textContent=secondNumber;            
+        decimalIsPresentSecondNumber=true;
+        console.log('decimal is placed for second number before any digits')                                
+    } else if(decimalIsPresentSecondNumber==false && secondNumber!=undefined) { // second number being typed can add its decimal
+        secondNumber+='.';
+        displayValue.textContent=secondNumber;
+        decimalIsPresentSecondNumber=true;
+        console.log('second number being typed can add its decimal')                                
+    }
+    
+});
+
+// clear calculator display and all variable values
+let allClearButton = document.getElementById('AC');
+
+allClearButton.addEventListener('click', function(){
+    allClear();
+});
+
+
